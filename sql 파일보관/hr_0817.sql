99a6475658d8fc510980cb49a887bbb90e6e045c
GYUHWAN SEOK <58979638+truespring@users.noreply.github.com>
2020-08-17 17:52:16 +0900

    Add files via upload

--- sql 파일보관/hr_0817.sql
+++ sql 파일보관/hr_0817.sql
@@ -1,3 +1,8 @@
+CREATE sequence seq_user
+nocache; -- 컴퓨터를 껏다 키면 캐시가 사라지기 때문에 막아줘야함 시퀀스 생성
+
+SELECT * FROM t_user;
+
 CREATE TABLE t_user(
  i_user number primary key,
  user_id varchar2(30) not null,
@@ -8,3 +13,5 @@ CREATE TABLE t_user(
  r_dt date default sysdate,
  m_dt date default sysdate
 );
+
+commit;
