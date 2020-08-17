679c9e4cd3c47d9a6544f8cd095ff2aa634a2ecb
GYUHWAN SEOK <58979638+truespring@users.noreply.github.com>
2020-08-17 14:03:08 +0900

    새로운 테이블 작성

---
+++ sql 파일보관/hr_0817.sql
@@ -0,0 +1,10 @@
+CREATE TABLE t_user(
+ i_user number primary key,
+ user_id varchar2(30) not null,
+ user_pw varchar2(100) not null,
+ nm varchar2(20) not null,
+ email varchar2(40),
+ profile_img varchar2(50),
+ r_dt date default sysdate,
+ m_dt date default sysdate
+);
